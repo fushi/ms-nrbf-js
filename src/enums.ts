a40cf07f60aa973292b7862f48e9ad1c67d5de82
John Towler <jtowler@genasi.org>
2020-05-04 00:42:37 -0700

    Initial commit. Plenty of things still broken, but time to officially track them, and release this into the semi-wild.

---
+++ src/enums.ts
@@ -0,0 +1,41 @@
+export const BINARY_TYPE_ENUM = {
+  Primitive: 0,
+  String: 1,
+  Object: 2,
+  SystemClass: 3,
+  Class: 4,
+  ObjectArray: 5,
+  StringArray: 6,
+  PrimitiveArray: 7
+}
+
+export const PRIMITIVE_TYPE_ENUM = {
+  Boolean: 1,
+  Byte: 2,
+  Char: 3,
+  Decimal: 5,
+  Double: 6,
+  Int16: 7,
+  Int32: 8,
+  Int64: 9,
+  SByte: 10,
+  Single: 11,
+  TimeSpan: 12,
+  DateTime: 13,
+  UInt16: 14,
+  UInt32: 15,
+  UInt64: 16,
+  Null: 17,
+  String: 18
+}
+
+export const RECORD_TYPE_ENUM = {
+  SerializedStreamHeader: 0,
+  ClassWithId: 1,
+  ClassWithMembersAndTypes: 5,
+  BinaryObjectString: 6,
+  MemberReference: 9,
+  ObjectNull: 10,
+  MessageEnd: 11,
+  BinaryLibrary: 12
+}
