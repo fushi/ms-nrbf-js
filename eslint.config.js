cb4f68184ad328afdbb57453c20b39d3b2243ca6
John Towler <jtowler@genasi.org>
2026-07-16 20:55:56 -0700

    Add stylistic linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,30 @@
-import tseslint from "typescript-eslint";
+import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        project: "./tsconfig.lint.json",
+        project: './tsconfig.lint.json',
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
+			'prefer-const': 'error',
+      'sort-keys': 'error',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': 'error',
       // Numeric enums in TypeScript 6 are transparent to `number`; comparing
       // a raw byte read to an enum member is correct and intentional here.
-      "@typescript-eslint/no-unnecessary-type-assertion": "off",
+      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     },
   },
   {
-    ignores: ["dist/", "eslint.config.js"],
+    ignores: ['dist/', 'eslint.config.js'],
   }
 );
